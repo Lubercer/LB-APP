bfbb8fae18e88c988521cda4c2f14fcfd16c6e6c
Luna Bercernelo <lunabercernelo@MacBook-Pro-de-Luna.local>
2022-08-17 13:28:12 -0300

    Se implemento navbar con bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar2 from './components/Navbar';
 
 const App = () => {
 
   const persona = {nombre: "ONLY GIRLS TATTOO APP", creador: "Luna Bercernelo"};
   return (
     <div className="App">
+      <Navbar2/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
